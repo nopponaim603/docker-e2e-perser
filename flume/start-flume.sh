9eefcd9572f754714e212b10ff9d6777f967e419
AIMMY-CAMTPC\noppo <noppon.w@cmu.ac.th>
2022-09-12 15:36:30 +0700

    Test Setup v2. add 5 config diffrenct post but  same bind flume and same send to kafka. Add CMD create 2 agent.

--- flume/start-flume.sh
+++ flume/start-flume.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 FLUME_CONF_DIR="/opt/flume/conf"
-FLUME_AGENT_NAME="docker"
+FLUME_AGENT1_NAME="docker1"
+FLUME_AGENT2_NAME="docker2"
 
 [[ -d "${FLUME_CONF_DIR}"  ]]  || { echo "Flume config file not mounted in /opt/flume-config";  exit 1; }
-[[ -z "${FLUME_AGENT_NAME}" ]] && { echo "FLUME_AGENT_NAME required"; exit 1; }
+[[ -z "${FLUME_AGENT1_NAME}" ]] && { echo "FLUME_AGENT_NAME required"; exit 1; }
 
-echo "Starting flume agent : ${FLUME_AGENT_NAME}"
+echo "Starting flume agent : ${FLUME_AGENT1_NAME}"
 
 flume-ng agent \
-  -n ${FLUME_AGENT_NAME} \
+  -n ${FLUME_AGENT1_NAME} \
   -c ${FLUME_CONF_DIR} \
   -f ${FLUME_CONF_DIR}/flume.conf \
-  -Dflume.root.logger=DEBUG,console \
+  -Dflume.root.logger=INFO,console \
 
